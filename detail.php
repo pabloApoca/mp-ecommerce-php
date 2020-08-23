306986c26a17224309f33c722ff8b36d9445c9d0
Pablo Lopez <pablo_mtv@hotmail.com>
2020-08-23 02:53:36 -0300

    Sume el checkout, redireccione el boton de procesar pago y elimine pagar.php

--- detail.php
+++ detail.php
@@ -1,3 +1,24 @@
+<?php
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+// Agrega credenciales
+
+MercadoPago\SDK::setAccessToken('TEST-8454724880295293-082303-46602d4a67385730495a40c59c7c089f-630199441');
+
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
+
+
+$item = new MercadoPago\Item();
+$item->title = $_POST['title'] ;
+$item->quantity = $_POST['unit'];
+$item->unit_price = $_POST['price'];
+$preference->items = array($item);
+$preference->save();
+
+
+?>
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
@@ -130,7 +151,13 @@
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
+                                   
+                                   <form action="https://pablomlopez-mp-ecommerce-php.herokuapp.com/" method="POST">
+								        <script
+								        src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+								        data-preference-id="<?php echo $preference->id; ?>">
+								        </script>
+								    </form>
                                 </div>
                             </div>
                         </div>
